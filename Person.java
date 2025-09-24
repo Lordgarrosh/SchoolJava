b43615fe6c2c6df91a385d199a1f1c21e79f4b0f
LordGarrosh <alexmenethil8@gmail.com>
2025-09-24 19:02:02 +0800

    Suicide Java

---
+++ Person.java
@@ -0,0 +1,22 @@
+public class Person {
+  private String name;
+  private String contactNumber;
+  
+  Person (String name, String contactNumb) {
+    this.name = name;
+    this.contactNumb = contactNumb;
+  }
+  
+  //getters
+  public String getName(){
+    return name;
+  }
+  public String getContactNumb() {
+    return contactNumb;
+  }
+  
+  //setters 
+  public void setName(String name) {
+    
+  }
+}
