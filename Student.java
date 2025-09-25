6474669d574bc98a4039c3dc9a065cce3c248199
LordGarrosh <alexmenethil8@gmail.com>
2025-09-25 22:55:12 +0800

    wtf

---
+++ Student.java
@@ -0,0 +1,26 @@
+ class Student extends Person{
+   private String program;
+   private int yearLevel;
+   
+   Student(String name, String contactNumb, String program, int level) {
+     super(name, contactNumb);
+     this.program = program;
+     this.yearLevel = yearLevel;
+   }
+   
+   // getters
+   public String getProgram() {
+     return program;
+   }
+   public int getYearLevel() {
+     return yearLevel;
+   }
+   
+   //setters 
+   
+   public void setProgram(String program) {
+     this.program = program;
+   }
+   
+   public 
+}
